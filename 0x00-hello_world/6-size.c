4fe19e418b2371d2cbb1df78d0e0dd2e07034f36
Teresia Wambui <shikuwambui@gmail.com>
2023-02-16 12:05:57 -0800

    size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,5 +13,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	return (0)
+	return (0);
 }
