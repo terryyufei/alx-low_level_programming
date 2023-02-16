62aaa95b2a0a81c7a2cfe780089bf77f032308b8
Teresia Wambui <shikuwambui@gmail.com>
2023-02-16 12:36:24 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+
+return (0);
 }	
