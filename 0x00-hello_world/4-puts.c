3c034be6fe2857b21964d04907802d1d76dd9077
Teresia Wambui <shikuwambui@gmail.com>
2023-02-16 06:30:43 -0800

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multlingual puzzle");
+
+	return (0);
+}	
