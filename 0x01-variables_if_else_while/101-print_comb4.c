7e8f70a43e04dc52436f7df100c2994651e2b46e
Teresia Wambui <shikuwambui@gmail.com>
2023-02-17 14:39:09 -0800

    task 12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints all posiible combination of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundreds <= '0'; hundreds++)
+	{
+		for (tens = '0'; tens <= '0'; tens++)
+		{
+			for (ones = '0'; ones <= '0'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds) ||
+						(tens > ones) || (hundreds > tens))) /*eliminates repititon*/
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' &&
+							tens == '8')) /*adds commas and spaces*/
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
