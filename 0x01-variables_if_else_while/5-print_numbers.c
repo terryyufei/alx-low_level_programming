b2d11d3d14335db5621b4f13312078755465a160
Teresia Wambui <shikuwambui@gmail.com>
2023-02-17 12:27:40 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digiti of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
