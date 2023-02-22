90907795b9103aff6a74114650805ffc7ff4c529
Teresia Wambui <shikuwambui@gmail.com>
2023-02-22 11:13:52 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 
 /**
  * main - tests function that prints if the interger is positive or negative
- * return: 0
+ * Return: 0
  */
 
-int main(void)	/* @i is the interger to be checked*/
+int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
 	positive_or_negative(i);
 
 	return (0);
