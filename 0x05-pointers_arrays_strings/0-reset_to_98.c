e6b155470026592d30dec0ac4d4a77dd06d57a29
Teresia Wambui <shikuwambui@gmail.com>
2023-02-27 09:33:09 -0800

    task zero

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function declaration
+ * @n: pointer interger
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this readme contains tasks on pinters, arrays and strings project
