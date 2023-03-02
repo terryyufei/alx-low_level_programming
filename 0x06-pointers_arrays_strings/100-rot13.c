69065b25425e2d6f1ca48bee9f28edcf4c119110
Teresia Wambui <shikuwambui@gmail.com>
2023-03-02 03:53:46 -0800

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encode using rot13
+ * @s: variable
+ * Return: Always 0 (Success)
+ */
+char *rot13(char *s)
+{
+	int i = 0, i2 = 0;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alpha2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + i) != 0)
+	{
+		for (i2 = 0; i2 <= 52; i2++)
+		{
+			if (*(s + i) == alpha[i2])
+			{
+				*(s + i) = alpha2[i2];
+				break;
+			}
+		}
+				i++;
+	}
+		return (s);
+}
+
