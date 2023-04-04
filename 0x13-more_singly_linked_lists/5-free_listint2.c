ab66b4ffb078137762554b283b3030e29e6fad60
Terry Wambui <shikuwambui@gmail.com>
2023-04-04 19:15:07 +0300

    free this one too

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: double pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *list;
+
+	if (head != NULL)
+	{
+		list = *head;
+		*head =(*head)->next;
+		free(list);
+	}
+}
