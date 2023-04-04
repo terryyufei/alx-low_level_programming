e35eba5e20b35e18a3f5490438d32c3427e1fd14
Terry Wambui <shikuwambui@gmail.com>
2023-04-04 20:21:41 +0300

    the node is at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,16 +9,19 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
+	
 
 	if (head == NULL)
 		return (NULL);
 
-	for (i = 0; i < index; i++)
+	while (head)
 	{
+		if (i == index)
+			return (head);
+		i++;
 		head = head->next;
 	}
-
-	return (head);
+	return (NULL);
 }
 
