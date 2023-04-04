8d5ddb934da848d9b135762145f56154a7e14891
Terry Wambui <shikuwambui@gmail.com>
2023-04-04 19:19:08 +0300

    free this one too

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *list;
 
-	if (head != NULL)
+	while (*head != NULL)
 	{
 		list = *head;
-		*head =(*head)->next;
+		*head = (*head)->next;
 		free(list);
 	}
 }
