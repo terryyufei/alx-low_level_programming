128e8b9b05a8e1e4bf9f22c4e03e41c56065cc61
Terry Wambui <shikuwambui@gmail.com>
2023-04-10 21:06:54 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,58 +1,73 @@
 #include "main.h"
-#include <errno.h>
-
-#define BUFFER_SIZE 1024
+#define BUF_SIZE 1024
 
 /**
- * main - program that copies the contents of a file to another
- * @argc: number of arguments
- * @argv: pointer to array of arguments
- *
- * Return: 0
- */
+* main - main
+* @argc: number of arguments
+* @argv: a pointer point to the array of arguments
+* Return: Always 0
+**/
 
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, res0, res1;
+	int f0, f1, res0, res1;
 	char *buffer;
 
-	if (argc != 3) /*checks no of arguments passed*/
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = malloc(sizeof(char) * BUFFER_SIZE);
+	buffer = malloc(sizeof(char) * BUF_SIZE);
 	if (!buffer)
 		return (0);
 
-	fd_from = open(argv[1], O_RDONLY);
-	error_98(fd_from, buffer, argv[1]);
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	error_99(fd_to, buffer, argv[2]);
+	f1 = open(argv[1], O_RDONLY);
+	error_98(f1, buffer, argv[1]);
+	f0 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	error_99(f0, buffer, argv[2]);
 	do {
-		res0 = read(fd_from, buffer, BUFFER_SIZE);
+		res0 = read(f1, buffer, BUF_SIZE);
 		if (res0 == 0)
 			break;
 		error_98(res0, buffer, argv[1]);
-		res1 = write(fd_to, buffer, res0);
+		res1 = write(f0, buffer, res0);
 		error_99(res1, buffer, argv[2]);
-	} while (res1 >= BUFFER_SIZE);
-	res0 = close(fd_to);
+	} while (res1 >= BUF_SIZE);
+	res0 = close(f0);
+	error_100(res0, buffer);
+	res0 = close(f1);
 	error_100(res0, buffer);
 	free(buffer);
 	return (0);
 }
 
 /**
+* error_98 - checks error 98
+* @f0: the value to check
+* @buffer: the buffer
+* @argv: argument
+**/
+void error_98(int f0, char *buffer, char *argv)
+{
+
+	if (f0 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv);
+		free(buffer);
+		exit(98);
+	}
+}
+
+/**
 * error_99 - checks error 99
-* @fd_to: value to check
+* @f0: value to check
 * @buffer: the buffer
 * @argv: argument
 */
-void error_99(int fd_to, char *buffer, char *argv)
+void error_99(int f0, char *buffer, char *argv)
 {
-	if (fd_to < 0)
+	if (f0 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
 		free(buffer);
@@ -61,14 +76,14 @@ void error_99(int fd_to, char *buffer, char *argv)
 }
 /**
 * error_100 - checks error 100
-* @fd_to: the value to check
+* @f0: the value to check
 * @buffer: the buffer
 */
-void error_100(int fd_to, char *buffer)
+void error_100(int f0, char *buffer)
 {
-	if (fd_to < 0)
+	if (f0 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", f0);
 		free(buffer);
 		exit(100);
 	}
