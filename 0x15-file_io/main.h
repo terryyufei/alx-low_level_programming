e3d9b59f4ae561298028a22ee8eabea8aab4b5bc
Terry Wambui <shikuwambui@gmail.com>
2023-04-10 21:01:00 +0300

    prototypes live here

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,10 +5,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_98(int fd_to, char *buffer, char *argv);
+void error_99(int fd_to, char *buffer, char *argv);
+void error_100(int fd_t0, char *buffer);
 
 #endif
