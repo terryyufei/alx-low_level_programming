8ef3ccd08a556433ee3ce1365998658e3852ece9
Terry Wambui <shikuwambui@gmail.com>
2023-04-10 16:24:13 +0300

    a function that appends

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (!text_content)
 		return (1);
 
 	len = strlen(text_content);
