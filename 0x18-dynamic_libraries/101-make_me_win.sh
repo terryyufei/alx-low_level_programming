06f4460fab8e501585b5788ef4e984c93b175288
Terry Wambui <shikuwambui@gmail.com>
2023-05-22 18:11:13 +0300

    win the jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/terryyufei/101-make_me_win/raw/main/libwinner.so
+wget -P /tmp https://github.com/terryyufei/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libwinner.so
 export LD_PRELOAD=/tmp/libwinner.so
