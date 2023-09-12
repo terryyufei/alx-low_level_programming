a696450a717e35c6904e7d1a87388ea78c69a319
Terry Wambui <shikuwambui@gmail.com>
2023-09-12 22:46:22 +0300

    Exponential search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for the value to be sorted
+ * _binary_search - searches for the value to be sorted
  * @array: array of integers
  * @left: left of element
  * @right: right of element
