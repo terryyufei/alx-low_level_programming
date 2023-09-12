ebdb47b12bb1126c3be460f1ab4a5ab571ee6daf
Terry Wambui <shikuwambui@gmail.com>
2023-09-12 23:34:33 +0300

    Linear search in a skip list

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of intergers
+ * @list: pointer to the head of the skip
+ * @value: value to search for
+ * Return: pointer to the first node
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node, *jump;
+
+	if (list == NULL)
+		return (NULL);
+
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
+	{
+		node = jump;
+		if (jump->express != NULL)
+		{
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+				   jump->index, jump->n);
+		}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+		   node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
